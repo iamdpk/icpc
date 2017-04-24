181e20420b6b04cbc8334f9262ccbe72cc3714cf
iamdpk <maildeepakmohan@gmail.com>
2017-04-24 20:23:37 +0530

    Update playonwords.cpp

--- dp/playonwords.cpp
+++ dp/playonwords.cpp
@@ -1,6 +1,10 @@
 /*codechef medium
   i think its somekind of dp
   dp[prefix][lastletter] but we are dealing with permutaitions so prefix doesnt makes sense
+  
+  model it as a graph problem??
+  names are nodes connect to nodes with same name //wrong
+  abcd...z are nodes
 
 */
 #include<iostream>
