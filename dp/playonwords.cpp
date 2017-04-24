3e935d571cc1ebe1d324c719b222551e403e5820
iamdpk <maildeepakmohan@gmail.com>
2017-04-24 19:52:31 +0530

    Update playonwords.cpp

--- dp/playonwords.cpp
+++ dp/playonwords.cpp
@@ -1,1 +1,7 @@
+/*codechef medium
+  i think its somekind of dp
+  dp[prefix][lastletter] but we are dealing with permutaitions so prefix doesnt makes sense
 
+*/
+#include<iostream>
+using namespace std;
